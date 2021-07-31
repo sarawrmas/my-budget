dd25f572a470277cc432e125b33fcf9cce70d5ed
Sara <sara.m.adamski@gmail.com>
2021-07-31 11:33:02 -0500

    update correct files for service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,20 +3,20 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./js/index.js",
-  "./js/idb.js",
-  "./assets/css/styles.css",
-  "./assets/icons/icon-72x72.png",
-  "./assets/icons/icon-96x96.png",
-  "./assets/icons/icon-128x128.png",
-  "./assets/icons/icon-144x144.png",
-  "./assets/icons/icon-152x152.png",
-  "./assets/icons/icon-192x192.png",
-  "./assets/icons/icon-384x384.png",
-  "./assets/icons/icon-512x512.png"
+  "/",
+  "/index.html",
+  // "/manifest.json",
+  "/js/index.js",
+  // "/js/idb.js",
+  "/css/styles.css",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png"
 ]
 
 self.addEventListener('install', function(e) {
